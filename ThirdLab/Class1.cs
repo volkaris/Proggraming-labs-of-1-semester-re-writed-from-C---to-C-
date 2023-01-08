15a04f33eab7fe1e2be81b70b67e54034c616e34
Ilya <110740644+volkaris@users.noreply.github.com>
2023-01-08 20:37:56 +0300

    Add files via upload

---
+++ ThirdLab/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class BankAccount
+    {
+        //public string AccountNumber { get; set; }
+        dynamic money;
+        dynamic percent;
+        BankAccount(int money, int percent)
+        {
+            this.percent = percent;
+            this.money = money;
+        }
+    }
+    
+}
+
